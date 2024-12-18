cce5c9452f6d7c8f482e20e44b4d7876749fbfc6
Johan Nicolin <johan.nicolin@gmail.com>
2024-12-18 16:30:08 +0000

    Code ready for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-z3%spjk!(zho3w-mr1g(hj+z7ffry36yz$+7(slx1s(h63w$n7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-jnicolin-mydjangoblog-nczm59lk6th.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-jnicolin-mydjangoblog-nczm59lk6th.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
